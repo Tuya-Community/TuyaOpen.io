5e4ab4f31f164c2d8b61d34bf72eedf3ca5bf860
Hsuan Han Lai <huate.lai@tuya.com>
2025-11-04 14:06:45 +0800

    toc wrapping

--- src/theme/TOC/index.js
+++ src/theme/TOC/index.js
@@ -1,34 +1,55 @@
 import BrowserOnly from '@docusaurus/BrowserOnly'
 import { useDoc } from '@docusaurus/plugin-content-docs/client'
 import TOC from '@theme-original/TOC'
+import React from 'react'
+
+// Component that uses useDoc hook - must be called unconditionally
+function GitpodButton() {
+  const { metadata } = useDoc()
+  const { editUrl } = metadata
+
+  if (!editUrl) {
+    return null
+  }
+
+  return (
+    <a
+      style={{
+        display: 'block',
+        marginBottom: '1rem',
+      }}
+      href={`https://gitpod.io/#${editUrl.replace('edit', 'blob')}`}
+      target="_blank"
+      rel="noreferrer noopener"
+    >
+      <img src="https://gitpod.io/button/open-in-gitpod.svg" alt="Open in Gitpod" />
+    </a>
+  )
+}
 
 export default function TOCWrapper(props) {
   return (
     <>
-      <BrowserOnly>
+      <BrowserOnly fallback={null}>
         {() => {
-          const pathname = window.location.pathname
-
-          if (!pathname.startsWith('/docs')) {
+          if (typeof window === 'undefined') {
             return null
           }
 
-          const { metadata } = useDoc()
-          const { editUrl } = metadata
+          const pathname = window.location.pathname
 
-          return (
-            <a
-              style={{
-                display: 'block',
-                marginBottom: '1rem',
-              }}
-              href={`https://gitpod.io/#${editUrl.replace('edit', 'blob')}`}
-              target="_blank"
-              rel="noreferrer noopener"
-            >
-              <img src="https://gitpod.io/button/open-in-gitpod.svg" alt="Open in Gitpod" />
-            </a>
-          )
+          // Check for locale-aware docs paths: /docs/, /en/docs/, /zh/docs/
+          // Match patterns like /docs, /docs/, /docs/..., /en/docs, /en/docs/, /en/docs/..., /zh/docs, /zh/docs/, /zh/docs/...
+          const isDocsPage = /^\/(en\/docs|zh\/docs|docs)(\/|$)/.test(pathname)
+
+          // Only render GitpodButton (which uses useDoc) when we're on a docs page
+          // This ensures the hook is only called when DocProvider is available
+          if (!isDocsPage) {
+            return null
+          }
+
+          // GitpodButton will call useDoc() unconditionally, but we only render it on docs pages
+          // return <GitpodButton />
         }}
       </BrowserOnly>
       <TOC {...props} />
