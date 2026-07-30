6b4e1c2c6a45ee4c799b939b9aee7de1a93f334c
Hsuan Han Lai <hsuan.han.lai@gmail.com>
2026-07-30 11:24:18 +0800

    alt

--- src/pages/discourse-test.js
+++ src/pages/discourse-test.js
@@ -1,12 +1,12 @@
-import DiscourseComments from '@site/src/components/DiscourseComments'
+import DiscourseTopicsList from '@site/src/components/DiscourseTopicsList'
 import Layout from '@theme/Layout'
 import React from 'react'
 
 export default function DiscourseTestPage() {
   return (
-    <Layout title="Discourse Embed Test" description="Test page for Discourse embedding">
-      <div style={{ width: '100%', minHeight: '100vh' }}>
-        <DiscourseComments embedHeight="calc(100vh - 60px)" />
+    <Layout title="Discourse Topics" description="Discourse topics embedded via d-topics-list">
+      <div style={{ width: '100%', minHeight: '100vh', padding: '1rem' }}>
+        <DiscourseTopicsList perPage={20} />
       </div>
     </Layout>
   )
